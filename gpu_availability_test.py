5e89404dada49fe523f46418ee6798204b02bb55
ishworrsubedii <ishworhero21@gmail.com>
2023-06-27 15:26:13 +0545

    gpu availability checker

---
+++ gpu_availability_test.py
@@ -0,0 +1,9 @@
+import torch
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('GPU is available')
+    print('CUDA device:', torch.cuda.get_device_name(device))
+else:
+    device = torch.device('cpu')
+
